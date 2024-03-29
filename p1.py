4763f0bdbe73af0de2fc7d3d8eb336310357c296
milenayahutl <164988484+milenayahutl@users.noreply.github.com>
2024-03-29 12:26:36 +0300

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,35 @@
+
+f=open('C:\\Users\\student\\Desktop\\students.csv', encoding='utf8')
+a=f.readlines()
+a.pop(0)
+for i in range(len(a)):
+    a[i]=a[i].strip().split(',')
+    a[i][0]=int(a[i][0])
+    a[i][2] = int(a[i][2])
+    if a[i][4]!='None':
+        a[i][4]=int(a[i][4])
+k=0
+m=0
+d={}
+for i in range(len(a)):
+    if a[i][4]!='None':
+        clas=a[i][3]
+        score=a[i][4]
+        if clas not in d:
+            d[clas]=[score]
+        else:
+            d[clas]+=[score]
+for clas in d:
+    d[clas]=round(sum(d[clas])/len(d[clas]))
+for i in range(len(a)):
+    if a[i][4]=='None':
+        a[i][4]=d[a[i][3]]
+for i in range(len(a)):
+    if 'Хадаров Владимир' in a[i][1]:
+        print(f' Ты получил: {a[i][4]}, за проект - {a[i][2]}')
+        break
+f=open('students_new.csv','w', encoding='utf8')
+f.writelines('id,Name,titleProjiect_id,class,score\n')
+for x in a:
+    f.writelines(','.join(x)+'\n')
+f.close()
